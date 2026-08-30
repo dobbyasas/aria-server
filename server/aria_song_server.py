c43c853d06bf7089288ca57bbab3e1f9dfbdbb3c
dobbyasas <svejkovsky.kry@gmail.com>
2026-08-30 13:14:11 +0200

    Bump shared version to 1.8.2

--- server/aria_song_server.py
+++ server/aria_song_server.py
@@ -23,7 +23,7 @@ from urllib.request import Request, urlopen
 
 
 SONG_EXTENSIONS = {".mp3", ".m4a", ".aac", ".wav", ".flac"}
-ARIA_VERSION = "1.8.1"
+ARIA_VERSION = "1.8.2"
 CATALOG_INDEX_VERSION = 4
 CATALOG_REFRESH_INTERVAL_SECONDS = 10
 DEFAULT_PAGE_LIMIT = 100
